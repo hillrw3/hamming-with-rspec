29652cc419f1a9fb5d43c561a612684eb94dfd14
Rob Hill <robert.hill0122@gmail.com>
2014-06-26 09:29:11 -0600

    Hamming using classes

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,21 @@
 class DnaStrandPair
 
+  def initialize(strand1, strand2)
+    @strand1 = strand1
+    @strand2 = strand2
+  end
+
+  def hamming_distance
+    strand1_array = @strand1.split(//)
+    strand2_array = @strand2.split(//)
+    if strand1_array.length > strand2_array.length
+      strand1_array.pop
+    elsif strand2_array.length > strand1_array.length
+      strand2_array.pop
+    else
+    end
+    distance = strand1_array - strand2_array
+    p distance.length
+  end
+
 end
